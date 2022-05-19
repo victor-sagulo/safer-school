775d461cb7e5c7e8a2c12d7b9f4d62b99e3db53a
Victor <vlcampos921@gmail.com>
2022-05-18 21:51:25 -0300

    fix: add list one teacher service export on index do services

--- src/services/Teacher/index.ts
+++ src/services/Teacher/index.ts
@@ -1,4 +1,5 @@
 export * from "./createTeacher.service";
 export * from "./listTeachers.service";
+export * from "./listOneTeacher.service";
 export * from "./updateTeacher.service";
 export * from "./deleteTeacher.service";
