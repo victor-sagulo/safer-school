f83fcfc009ec849ccf300689f076a8c899411f6d
Victor <vlcampos921@gmail.com>
2022-05-19 09:31:43 -0300

    fix: update entities and some relatives controller

--- src/routes/Classroom/index.ts
+++ src/routes/Classroom/index.ts
@@ -6,7 +6,6 @@ const classroomRoutes = Router();
 
 classroomRoutes.get("/", ClassroomController.index);
 classroomRoutes.post("/", ClassroomController.store);
-
 classroomRoutes.get("/:id", validateIdParams, ClassroomController.show);
 classroomRoutes.patch("/:id", validateIdParams, ClassroomController.update);
 classroomRoutes.delete("/:id", validateIdParams, ClassroomController.delete);
