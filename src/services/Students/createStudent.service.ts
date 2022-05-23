f18b68659dd5984283d10aeff73ec3217d8c3742
Marcos <marcosmafeimachado@gmail.com>
2022-05-23 11:52:13 -0300

    fix: change the student creation yup schema

--- src/services/Students/createStudent.service.ts
+++ src/services/Students/createStudent.service.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from "../../data-source";
 import { Student } from "../../entities/Student";
+import { AppError } from "../../errors";
 import { StudentCreation } from "../../interfaces/Student/student.interface";
 
 export const createStudentService = async ({
@@ -9,9 +10,24 @@ export const createStudentService = async ({
 }: StudentCreation) => {
   const studentRepository = AppDataSource.getRepository(Student);
 
-  const student = new Student(name, birthDate, address);
+  try {
+    const student = new Student(name, birthDate, address);
 
-  await studentRepository.save(student);
+    await studentRepository.save(student);
 
-  return student;
+    return student;
+  } catch (err) {
+    if (err instanceof Error) {
+      if (
+        err.message ==
+        'invalid input syntax for type timestamp: "0NaN-NaN-NaNTNaN:NaN:NaN.NaN+NaN:NaN"'
+      ) {
+        throw new AppError(
+          400,
+          "Date must be in american format: month/day/year"
+        );
+      }
+      throw new AppError(400, err.message);
+    }
+  }
 };
