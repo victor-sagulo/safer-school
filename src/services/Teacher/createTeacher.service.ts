b995c29cf21135cd6399935ccd14d8eae08332dc
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-05-23 11:04:33 -0300

    fix: Removing structure trycatch

--- src/services/Teacher/createTeacher.service.ts
+++ src/services/Teacher/createTeacher.service.ts
@@ -9,26 +9,17 @@ export const createTeacherService = async ({
 }: TeacherCreation) => {
   const teacherRepository = AppDataSource.getRepository(Teacher);
 
-  try {
-    const findTeacher = await teacherRepository.findOneBy({
-      email: email,
-    });
+  const findTeacher = await teacherRepository.findOneBy({
+    email: email,
+  });
 
-    if (findTeacher) {
-      throw new AppError(409, "This email already exists");
-    }
-
-    const teacher = new Teacher(name, email);
-
-    await teacherRepository.save(teacher);
-
-    return teacher;
-  } catch (err) {
-    if (err instanceof AppError) {
-      throw new AppError(err.statusCode, err.message);
-    }
-    if (err instanceof Error) {
-      throw new AppError(400, err.message);
-    }
+  if (findTeacher) {
+    throw new AppError(409, "This email already exists");
   }
+
+  const teacher = new Teacher(name, email);
+
+  await teacherRepository.save(teacher);
+
+  return teacher;
 };
--- src/services/Teacher/updateTeacher.service.ts
+++ src/services/Teacher/updateTeacher.service.ts
@@ -9,28 +9,19 @@ export const updateTeacherService = async ({
 }: Partial<Teacher>) => {
   const teacherRepository = AppDataSource.getRepository(Teacher);
 
-  try {
-    const teacher = await teacherRepository.findOneBy({ id });
+  const teacher = await teacherRepository.findOneBy({ id });
 
-    if (!teacher) {
-      throw new AppError(404, "Teacher not found or doesn't exists");
-    }
-
-    const updatedTeacher = {
-      id: teacher.id,
-      name: name || teacher.name,
-      email: email || teacher.email,
-    };
-
-    await teacherRepository.update(teacher.id, updatedTeacher);
-
-    return updatedTeacher;
-  } catch (err) {
-    if (err instanceof AppError) {
-      throw new AppError(err.statusCode, err.message);
-    }
-    if (err instanceof Error) {
-      throw new AppError(400, err.message);
-    }
+  if (!teacher) {
+    throw new AppError(404, "Teacher not found or doesn't exists");
   }
+
+  const updatedTeacher = {
+    id: teacher.id,
+    name: name || teacher.name,
+    email: email || teacher.email,
+  };
+
+  await teacherRepository.update(teacher.id, updatedTeacher);
+
+  return updatedTeacher;
 };
